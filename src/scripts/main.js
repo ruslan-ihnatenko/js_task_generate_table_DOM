fad48a87589305a68a4464c913934035d7c5cdbd
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2025-01-10 02:58:03 +0100

    Fix 0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -359,16 +359,18 @@ console.log(people); // you can remove it
 
 const table = [...document.getElementsByClassName('dashboard')];
 
-for (const person of people) {
-  table[0].children[0].insertAdjacentHTML(
-    'beforeend',
-    `<tr>
-      <td>${person.name}</td>
-      <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
-      <td>${person.born}</td>
-      <td>${person.died}</td>
-      <td>${person.died - person.born}</td>
-      <td>${Math.ceil(person.died / 100)}</td>
-    </tr>`,
-  );
+if (table) {
+  for (const person of people) {
+    table[0].children[0].insertAdjacentHTML(
+      'beforeend',
+      `<tr>
+        <td>${person.name}</td>
+        <td>${person.sex === 'm' ? 'Male' : 'Female'}</td>
+        <td>${person.born}</td>
+        <td>${person.died}</td>
+        <td>${person.died - person.born}</td>
+        <td>${Math.ceil(person.died / 100)}</td>
+      </tr>`,
+    );
+  }
 }
